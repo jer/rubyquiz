0d5c7b45e09971f5e06b45329bc893e03d07d090
Jeremiah Wuenschel <jeremiah.wuenschel@gmail.com>
2014-08-31 15:11:04 -0700

    Quiz1: Added a formatter to create 5-letter groups of words

---
+++ lib/quiz1_solitaire.rb
@@ -0,0 +1,18 @@
+module Quiz1SolitaireCipher
+  # An implementation of the Solitaire Cipher from RubyQuiz #1
+  # http://rubyquiz.com/quiz1.html
+  class SolitaireCipher
+    def format_plaintext(plaintext)
+      plaintext.upcase!
+
+      plaintext.gsub!(/[^A-Z]/, '')
+
+      plaintext_arr = plaintext.scan(/.{1,5}/)
+
+      last_word = plaintext_arr[-1]
+      plaintext_arr[-1] = last_word.ljust(5, 'X') if last_word.length < 5
+
+      plaintext_arr.join(' ')
+    end
+  end
+end
---
+++ spec/lib/quiz1_solitaire_spec.rb
@@ -0,0 +1,25 @@
+require 'quiz1_solitaire'
+
+describe Quiz1SolitaireCipher::SolitaireCipher, '#format_plaintext' do
+  let(:cipher) { Quiz1SolitaireCipher::SolitaireCipher.new }
+  it 'capitalizes all letters' do
+    expect(cipher.format_plaintext('AbCdE')).to eq 'ABCDE'
+  end
+
+  it 'strips out non-alphabet characters' do
+    expect(cipher.format_plaintext('1abc23D4e')).to eq 'ABCDE'
+  end
+
+  it 'splits the string into 5-character groups' do
+    original = 'Code in Ruby, live longer!'
+    parsed = 'CODEI NRUBY LIVEL ONGER'
+    expect(cipher.format_plaintext(original)).to eq parsed
+  end
+
+  it 'pads the last character group with X' do
+    original = 'Code in Ruby, live long!'
+    parsed = 'CODEI NRUBY LIVEL ONGXX'
+    expect(cipher.format_plaintext(original)).to eq parsed
+    expect(cipher.format_plaintext('a')).to eq 'AXXXX'
+  end
+end
