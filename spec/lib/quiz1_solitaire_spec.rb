0e49c8632171c80c1c107dcad626abd352e03710
Jeremiah Wuenschel <jeremiah.wuenschel@gmail.com>
2014-09-08 22:27:23 -0700

    Rearranging the spec tests under contexts

--- spec/lib/quiz1_solitaire_spec.rb
+++ spec/lib/quiz1_solitaire_spec.rb
@@ -4,25 +4,31 @@ require 'spec_helper'
 require 'quiz1_solitaire'
 
 describe Quiz1SolitaireCipher::SolitaireCipher, '#format_plaintext' do
-  let(:cipher) { Quiz1SolitaireCipher::SolitaireCipher.new }
-  it 'capitalizes all letters' do
-    expect(cipher.format_plaintext('AbCdE')).to eq 'ABCDE'
+  context '#format_plaintext' do
+    let(:cipher) { Quiz1SolitaireCipher::SolitaireCipher.new }
+    it 'capitalizes all letters' do
+      expect(cipher.format_plaintext('AbCdE')).to eq 'ABCDE'
+    end
+
+    it 'strips out non-alphabet characters' do
+      expect(cipher.format_plaintext('1abc23D4e')).to eq 'ABCDE'
+    end
+
+    it 'splits the string into 5-character groups' do
+      original = 'Code in Ruby, live longer!'
+      parsed = 'CODEI NRUBY LIVEL ONGER'
+      expect(cipher.format_plaintext(original)).to eq parsed
+    end
+
+    it 'pads the last character group with X' do
+      original = 'Code in Ruby, live long!'
+      parsed = 'CODEI NRUBY LIVEL ONGXX'
+      expect(cipher.format_plaintext(original)).to eq parsed
+      expect(cipher.format_plaintext('a')).to eq 'AXXXX'
+    end
   end
 
-  it 'strips out non-alphabet characters' do
-    expect(cipher.format_plaintext('1abc23D4e')).to eq 'ABCDE'
-  end
-
-  it 'splits the string into 5-character groups' do
-    original = 'Code in Ruby, live longer!'
-    parsed = 'CODEI NRUBY LIVEL ONGER'
-    expect(cipher.format_plaintext(original)).to eq parsed
-  end
-
-  it 'pads the last character group with X' do
-    original = 'Code in Ruby, live long!'
-    parsed = 'CODEI NRUBY LIVEL ONGXX'
-    expect(cipher.format_plaintext(original)).to eq parsed
-    expect(cipher.format_plaintext('a')).to eq 'AXXXX'
+  context '#make_numeric' do
+    let(:cipher) { Quiz1SolitaireCipher::SolitaireCipher.new }
   end
 end
